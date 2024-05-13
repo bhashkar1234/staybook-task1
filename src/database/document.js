d9eccbdf96f261e81bed8f03713cc31111e152bf
bhashkar1234 <111336488+bhashkar1234@users.noreply.github.com>
2024-05-13 22:14:24 +0530

    Update document.js

--- src/database/document.js
+++ src/database/document.js
@@ -1,68 +1,68 @@
-/*
-  sample json data for the firebase document
-  to be saved and updated via api requests
-*/
 
 class HotelDetails {
-  hotelName = ""; // string
-  hotelEmailId = ""; // string
-  hotelContactNumber = 0; // number
-  hotelLandmark = ""; // string
-  hotelAddress = ""; // string
-  hotelStarRating = 0; // number
-  hotelImageUrl = ""; // string
+  constructor() {
+    this.hotelName = ""; // string
+    this.hotelEmailId = ""; // string
+    this.hotelContactNumber = 0; // number
+    this.hotelLandmark = ""; // string
+    this.hotelAddress = ""; // string
+    this.hotelStarRating = 0; // number
+    this.hotelImageUrl = ""; // string
 
-  hotelCity = ""; // string
-  hotelState = ""; // string
-  hotelPincode = ""; // string
+    this.hotelCity = ""; // string
+    this.hotelState = ""; // string
+    this.hotelPincode = ""; // string
 
-  // example
-  // hotelName = "Jai Balaji";
-  // cityName = "New Delhi";
-  // stateName = "Delhi";
-  // change the name into dashified lowercase version then use it in the slugs
-  // you can use dashify (https://www.npmjs.com/package/dashify) npm package for this behaviour or create your own
+    // example
+    // this.hotelName = "Jai Balaji";
+    // this.cityName = "New Delhi";
+    // this.stateName = "Delhi";
+    // change the name into dashified lowercase version then use it in the slugs
+    // you can use dashify (https://www.npmjs.com/package/dashify) npm package for this behavior or create your own
 
-  // hotelSlugsDetails.hotel = `staybook-hotel-${hotelName}-${cityName}`
-  // hotelSlugsDetails.hotelCity = `hotels-in-${cityName}`
-  // hotelSlugsDetails.hotelState = hotels-in-${stateName}
+    // this.hotelSlugsDetails.hotel = `staybook-hotel-${this.hotelName}-${this.cityName}`
+    // this.hotelSlugsDetails.hotelCity = `hotels-in-${this.cityName}`
+    // this.hotelSlugsDetails.hotelState = `hotels-in-${this.stateName}`
 
-  hotelSlugsDetails = {
-    hotel: "", // string
-    hotelCity: "", // string
-    hotelState: "", // string
-  };
+    this.hotelSlugsDetails = {
+      hotel: "", // string
+      hotelCity: "", // string
+      hotelState: "", // string
+    };
 
-  hotelLongitude = 0; // number
-  hotelLatitude = 0; // number
-  hotelMapUrl = ""; //string
+    this.hotelLongitude = 0; // number
+    this.hotelLatitude = 0; // number
+    this.hotelMapUrl = ""; //string
 
-  hotelPaymentOption = {
-    postpaid: false, // boolean
-    prepaid: false, // boolean
-    partial: false, // boolean
-  };
+    this.hotelPaymentOption = {
+      postpaid: false, // boolean
+      prepaid: false, // boolean
+      partial: false, // boolean
+    };
 
-  hotelImagesList = [
-    {
-      imageId: "", // string
-      imageUrl: "", // string
-      imageTitle: "", // string
-    },
-    {
-      imageId: "", // string
-      imageUrl: "", // string
-      imageTitle: "", // string
-    },
-    {
-      imageId: "", // string
-      imageUrl: "", // string
-      imageTitle: "", // string
-    },
-    {
-      imageId: "", // string
-      imageUrl: "", // string
-      imageTitle: "", // string
-    },
-  ];
+    this.hotelImagesList = [
+      {
+        imageId: "", // string
+        imageUrl: "", // string
+        imageTitle: "", // string
+      },
+      {
+        imageId: "", // string
+        imageUrl: "", // string
+        imageTitle: "", // string
+      },
+      {
+        imageId: "", // string
+        imageUrl: "", // string
+        imageTitle: "", // string
+      },
+      {
+        imageId: "", // string
+        imageUrl: "", // string
+        imageTitle: "", // string
+      },
+    ];
+  }
 }
+
+module.exports = HotelDetails;
