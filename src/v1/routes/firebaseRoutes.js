a2c95e83f3f4d016eee50b647719d8134bb6f8fa
bhashkar1234 <111336488+bhashkar1234@users.noreply.github.com>
2024-05-13 22:18:36 +0530

    Update firebaseRoutes.js

--- src/v1/routes/firebaseRoutes.js
+++ src/v1/routes/firebaseRoutes.js
@@ -1,12 +1,26 @@
+
 const { Router } = require("express");
 const {
-  getAllFirebaseDocuments,
+  createHotelDocument,
+  getAllHotelDocuments,
+  updateHotelDocument,
+  deleteHotelDocument,
 } = require("../controllers/firebaseController");
 
 // initialise api version1 express router
 const v1Router = Router();
 
-// sample get route to get all the documents from firebase
-v1Router.get("/", getAllFirebaseDocuments);
+// Route to get all hotel documents
+v1Router.get("/get", getAllHotelDocuments);
+
+// Route to create a new hotel document
+v1Router.post("/create", createHotelDocument);
+
+// Route to update an existing hotel document
+v1Router.put("/hotels/:id", updateHotelDocument);
+
+// Route to delete a hotel document
+v1Router.delete("/hotels/:id", deleteHotelDocument);
 
 module.exports = v1Router;
+
